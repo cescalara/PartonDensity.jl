374895fdc8e567cbe2eb3d48f23e0c69e8830c77
Francesca Capel <capel.francesca@gmail.com>
2022-01-06 15:50:09 +0100

    Add docs for prior BAT implementation

--- docs/make.jl
+++ docs/make.jl
@@ -8,17 +8,20 @@ using Literate
 gen_content_dir = joinpath(@__DIR__, "src")
 
 pdf_parametrisation_src = joinpath(@__DIR__, "..", "examples", "pdf_parametrisation.jl")
-forward_model_src = joinpath(@__DIR__, "..", "examples", "forward_model.jl")
 transfer_matrix_src = joinpath(@__DIR__, "..", "examples", "transfer_matrix.jl")
+forward_model_src = joinpath(@__DIR__, "..", "examples", "forward_model.jl")
+prior_bat_src = joinpath(@__DIR__, "..", "examples", "prior_bat_implementation.jl")
 
 Literate.markdown(pdf_parametrisation_src, gen_content_dir, name="pdf_parametrisation")
-Literate.markdown(forward_model_src, gen_content_dir, name="forward_model")
 Literate.markdown(transfer_matrix_src, gen_content_dir, name="transfer_matrix")
+Literate.markdown(forward_model_src, gen_content_dir, name="forward_model")
+Literate.markdown(prior_bat_src, gen_content_dir, name="prior_bat_implementation")
 
 Introduction = "Introduction" => "index.md"
 
 Examples = "Examples" => ["pdf_parametrisation.md",
-                          "forward_model.md", "transfer_matrix.md"]
+                          "transfer_matrix.md", "forward_model.md",
+                          "prior_bat_implementation.md"]
 
 API = "API" => "api.md"
 
