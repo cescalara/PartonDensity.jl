96f325e1c4feb15705d6aa229fc1748a97eddf3c
Richard Hildebrandt <108278991+rdebrand@users.noreply.github.com>
2022-08-25 18:35:27 +0200

    simplify functions

--- src/Bernstein_forward_model.jl
+++ src/Bernstein_forward_model.jl
@@ -8,23 +8,7 @@ qcdnum_evolfg_ϵ_values = Vector{Float64}()
 splint_init_complete = false
 
 
-function forward_model(pdf_params::BernsteinPDFParams, qcdnum_params::QCDNUMParameters,
-                       splint_params::SPLINTParameters, quark_coeffs::QuarkCoefficients)
-    
-    forward_model_impl(pdf_params, qcdnum_params, splint_params, quark_coeffs)
-  
-end
-
-
-function forward_model(pdf_params::BernDirPDFParams, qcdnum_params::QCDNUMParameters,
-                       splint_params::SPLINTParameters, quark_coeffs::QuarkCoefficients)
-        
-    forward_model_impl(pdf_params, qcdnum_params, splint_params, quark_coeffs)
-  
-end
-
-
-function forward_model_impl(pdf_params::Any, qcdnum_params::QCDNUMParameters,
+function forward_model(pdf_params::Union{BernsteinPDFParams, BernDirPDFParams}, qcdnum_params::QCDNUMParameters,
                        splint_params::SPLINTParameters, quark_coeffs::QuarkCoefficients)
 
 
@@ -116,22 +100,8 @@ function forward_model_impl(pdf_params::Any, qcdnum_params::QCDNUMParameters,
     
 end
 
-
-function pd_write_sim(file_name::String, pdf_params::BernsteinPDFParams, sim_data::Dict{String, Any})
-
-    pd_write_sim_impl(file_name, pdf_params, sim_data)
       
-end
-    
-    
-function pd_write_sim(file_name::String, pdf_params::BernDirPDFParams, sim_data::Dict{String, Any})
-    
-    pd_write_sim_impl(file_name, pdf_params, sim_data)
-
-end
-    
-      
-function pd_write_sim_impl(file_name::String, pdf_params::Any, sim_data::Dict{String, Any})
+function pd_write_sim_impl(file_name::String, pdf_params::Union{BernsteinPDFParams, BernDirPDFParams}, sim_data::Dict{String, Any})
 
     h5open(file_name, "w") do fid
 
