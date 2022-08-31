7f4f830f99322f2617a3a3ebdc5c96c5b1650aee
Francesca Capel <capel.francesca@gmail.com>
2022-08-31 17:45:37 +0200

    Add K_q as free param and update tests

--- src/Bernstein_forward_model.jl
+++ src/Bernstein_forward_model.jl
@@ -9,23 +9,23 @@ splint_init_complete = false
 
 
 function forward_model(pdf_params::BernsteinPDFParams, qcdnum_params::QCDNUMParameters,
-                       splint_params::SPLINTParameters, quark_coeffs::QuarkCoefficients)
-    
+    splint_params::SPLINTParameters, quark_coeffs::QuarkCoefficients)
+
     forward_model_impl(pdf_params, qcdnum_params, splint_params, quark_coeffs)
-  
+
 end
 
 
 function forward_model(pdf_params::BernDirPDFParams, qcdnum_params::QCDNUMParameters,
-                       splint_params::SPLINTParameters, quark_coeffs::QuarkCoefficients)
-        
+    splint_params::SPLINTParameters, quark_coeffs::QuarkCoefficients)
+
     forward_model_impl(pdf_params, qcdnum_params, splint_params, quark_coeffs)
-  
+
 end
 
 
 function forward_model_impl(pdf_params::Any, qcdnum_params::QCDNUMParameters,
-                       splint_params::SPLINTParameters, quark_coeffs::QuarkCoefficients)
+    splint_params::SPLINTParameters, quark_coeffs::QuarkCoefficients)
 
 
     # Get input PDF function
@@ -41,10 +41,10 @@ function forward_model_impl(pdf_params::Any, qcdnum_params::QCDNUMParameters,
     if ϵ > 0.05
 
         @warn "QCDNUM.evolfg(): Spline issues detected" eps pdf_params
-        
+
     end
     push!(qcdnum_evolfg_ϵ_values, ϵ)
-    
+
     # Read spline addresses
     iaF2up = Int64(QCDNUM.dsp_uread(splint_params.spline_addresses.F2up))
     iaF2dn = Int64(QCDNUM.dsp_uread(splint_params.spline_addresses.F2dn))
@@ -54,12 +54,12 @@ function forward_model_impl(pdf_params::Any, qcdnum_params::QCDNUMParameters,
     iaFLdn = Int64(QCDNUM.dsp_uread(splint_params.spline_addresses.FLdn))
     iaF_eP = Int64(QCDNUM.dsp_uread(splint_params.spline_addresses.F_eP))
     iaF_eM = Int64(QCDNUM.dsp_uread(splint_params.spline_addresses.F_eM))
-    
+
     # Splines for structure functions
     QCDNUM.ssp_s2f123(iaFLup, pdf_loc, quark_coeffs.proup, 1, 0.0)
     QCDNUM.ssp_s2f123(iaF2up, pdf_loc, quark_coeffs.proup, 2, 0.0)
-    QCDNUM.ssp_s2f123(iaF3up, pdf_loc, quark_coeffs.valup, 3, 0.0)  
-    QCDNUM.ssp_s2f123(iaFLdn, pdf_loc, quark_coeffs.prodn, 1, 0.0)  
+    QCDNUM.ssp_s2f123(iaF3up, pdf_loc, quark_coeffs.valup, 3, 0.0)
+    QCDNUM.ssp_s2f123(iaFLdn, pdf_loc, quark_coeffs.prodn, 1, 0.0)
     QCDNUM.ssp_s2f123(iaF2dn, pdf_loc, quark_coeffs.prodn, 2, 0.0)
     QCDNUM.ssp_s2f123(iaF3dn, 1, quark_coeffs.valdn, 3, 0.0)
 
@@ -76,14 +76,14 @@ function forward_model_impl(pdf_params::Any, qcdnum_params::QCDNUMParameters,
 
     # Integrate over cross section
     nbins = size(xbins_M_begin)[1]
-    integ_xsec_ep = zeros(nbins);
-    integ_xsec_em = zeros(nbins);
+    integ_xsec_ep = zeros(nbins)
+    integ_xsec_em = zeros(nbins)
 
     for i in 1:nbins
         integ_xsec_ep[i] = QCDNUM.dsp_ints2(iaF_eP, xbins_M_begin[i], xbins_M_end[i],
-                                            q2bins_M_begin[i], q2bins_M_end[i], 318., 4)
+            q2bins_M_begin[i], q2bins_M_end[i], 318.0, 4)
         integ_xsec_em[i] = QCDNUM.dsp_ints2(iaF_eM, xbins_M_begin[i], xbins_M_end[i],
-                                            q2bins_M_begin[i], q2bins_M_end[i], 318., 4)
+            q2bins_M_begin[i], q2bins_M_end[i], 318.0, 4)
     end
 
     # Fold through response to get counts
@@ -102,36 +102,36 @@ function forward_model_impl(pdf_params::Any, qcdnum_params::QCDNUMParameters,
     counts_pred_em = zeros(nbins_out)
 
     for j in 1:nbins_out
-        
+
         for i in 1:nbins
 
-            counts_pred_ep[j] += TM_eP[i, j] * (1.0/K_eP[i]) * integ_xsec_ep[i]
-            counts_pred_em[j] += TM_eM[i, j] * (1.0/K_eM[i]) * integ_xsec_em[i]
-            
+            counts_pred_ep[j] += TM_eP[i, j] * (1.0 / K_eP[i]) * integ_xsec_ep[i]
+            counts_pred_em[j] += TM_eM[i, j] * (1.0 / K_eM[i]) * integ_xsec_em[i]
+
         end
 
     end
 
     return counts_pred_ep, counts_pred_em
-    
+
 end
 
 
-function pd_write_sim(file_name::String, pdf_params::BernsteinPDFParams, sim_data::Dict{String, Any})
+function pd_write_sim(file_name::String, pdf_params::BernsteinPDFParams, sim_data::Dict{String,Any})
 
-    pd_write_sim_impl(file_name, pdf_params, sim_data)
-      
-end
-    
-    
-function pd_write_sim(file_name::String, pdf_params::BernDirPDFParams, sim_data::Dict{String, Any})
-    
     pd_write_sim_impl(file_name, pdf_params, sim_data)
 
 end
-    
-      
-function pd_write_sim_impl(file_name::String, pdf_params::Any, sim_data::Dict{String, Any})
+
+
+function pd_write_sim(file_name::String, pdf_params::BernDirPDFParams, sim_data::Dict{String,Any})
+
+    pd_write_sim_impl(file_name, pdf_params, sim_data)
+
+end
+
+
+function pd_write_sim_impl(file_name::String, pdf_params::Any, sim_data::Dict{String,Any})
 
     h5open(file_name, "w") do fid
 
@@ -149,11 +149,12 @@ function pd_write_sim_impl(file_name::String, pdf_params::Any, sim_data::Dict{St
         truth_group["λ_g2"] = pdf_params.λ_g2
         truth_group["K_g"] = pdf_params.K_g
         truth_group["λ_q"] = pdf_params.λ_q
+        truth_group["K_q"] = pdf_params.K_q
         truth_group["seed"] = pdf_params.seed
         truth_group["weights"] = pdf_params.weights
         truth_group["θ"] = pdf_params.θ
         truth_group["param_type"] = pdf_params.param_type
-        
+
     end
 
     return nothing
--- src/parametrisation.jl
+++ src/parametrisation.jl
@@ -35,7 +35,8 @@ Full Dirichlet specification of input PDF parameters.
     λ_g1::Float64
     λ_g2::Float64
     K_g::Float64
-    λ_q::Float64   
+    λ_q::Float64
+    K_q::Float64
 end
 
 
@@ -54,6 +55,7 @@ Valence specification of input PDF parameters.
     λ_g2::Float64
     K_g::Float64
     λ_q::Float64
+    K_q::Float64
     seed::Integer = 0
     weights::Vector{Float64} = ones(7)
     θ::Vector{Float64} = get_dirichlet_samples(λ_u, K_u, λ_d, K_d, seed, weights)
@@ -68,9 +70,9 @@ according to the valence params. Relevant for
 `ValencePDFParams`.
 """
 function get_scaled_θ(λ_u::Float64, K_u::Float64, λ_d::Float64, K_d::Float64,
-                      θ_tmp::Vector{Float64})
+    θ_tmp::Vector{Float64})
 
-    
+
     A_u = 2 / sf.beta(λ_u, K_u + 1)
     A_d = 1 / sf.beta(λ_d, K_d + 1)
 
@@ -80,7 +82,7 @@ function get_scaled_θ(λ_u::Float64, K_u::Float64, λ_d::Float64, K_d::Float64,
     remaining = 1 - (I_u + I_d)
 
     return θ_tmp * remaining
-  
+
 end
 
 
@@ -91,15 +93,15 @@ Given valance shape parameters and weights, get samples θ
 that are correctly scaled. Relevant for `ValencePDFParams` 
 """
 function get_dirichlet_samples(λ_u::Float64, K_u::Float64, λ_d::Float64,
-                               K_d::Float64, seed::Integer, weights::Vector{Float64})
+    K_d::Float64, seed::Integer, weights::Vector{Float64})
 
-    Random.seed!(seed);
+    Random.seed!(seed)
     θ_tmp = rand(Dirichlet(weights))
 
     θ = get_scaled_θ(λ_u, K_u, λ_d, K_d, θ_tmp)
-    
+
     return θ
-    
+
 end
 
 
@@ -112,11 +114,11 @@ Beta function
 A_u is set by λ_u and K_u.
 """
 function x_uv_x(x::Float64, λ_u::Float64, K_u::Float64)
-    
+
     A_u = 2 / sf.beta(λ_u, K_u + 1)
-    
+
     return A_u * x^λ_u * (1 - x)^K_u
-    
+
 end
 
 
@@ -133,55 +135,55 @@ function x_dv_x(x::Float64, λ_d::Float64, K_d::Float64)
     A_d = 1 / sf.beta(λ_d, K_d + 1)
 
     return A_d * x^λ_d * (1 - x)^K_d
-    
+
 end
 
 """
-        x_g_x(x, λ_g1, λ_g2, K_g, w1, w2)
+        x_g_x(x, λ_g1, λ_g2, K_g, K_q, w1, w2)
 
 Momentum density of gluon component.
-    A_g1 x^λ_g1 (1 - x)^K_g + A_g2 x^λ_g2
+    A_g1 x^λ_g1 (1 - x)^K_g + A_g2 x^λ_g2 (1 - x)^K_q
 Amplitudes are set by weights `w1` and `w2`.
 """
 function x_g_x(x::Float64, λ_g1::Float64, λ_g2::Float64, K_g::Float64,
-               w1::Float64, w2::Float64)
-    
+    K_q::Float64, w1::Float64, w2::Float64)
+
     A_g1 = w1 / sf.beta(λ_g1 + 1, K_g + 1)
-    
+
     x_g1_x = A_g1 * x^λ_g1 * (1 - x)^K_g
 
-    A_g2 = w2 / sf.beta(λ_g2 + 1, 5 + 1)
-    
-    x_g2_x = A_g2 * x^λ_g2 * (1 - x)^5
+    A_g2 = w2 / sf.beta(λ_g2 + 1, K_q + 1)
+
+    x_g2_x = A_g2 * x^λ_g2 * (1 - x)^K_q
 
     return x_g1_x + x_g2_x
-    
+
 end
 
 """
-    x_q_x(x, λ_q, w)
+    x_q_x(x, λ_q, K_q, w)
 
 Momentum density of non-valence
 quark component.
-    A_q x^λ_q
+    A_q x^λ_q (1 - x)^K_q 
 Amplitude is set by weight `w`
 """
-function x_q_x(x::Float64, λ_q::Float64, w::Float64)
-    
-    A_q = (w / 2) / sf.beta(λ_q + 1, 5 + 1)
-    
-    return A_q * x^λ_q * (1 - x)^5
-    
+function x_q_x(x::Float64, λ_q::Float64, K_q::Float64, w::Float64)
+
+    A_q = (w / 2) / sf.beta(λ_q + 1, K_q + 1)
+
+    return A_q * x^λ_q * (1 - x)^K_q
+
 end
 
 
 """
-    x_total_x(x, λ_u, K_u, λ_d, K_d, λ_g1, λ_g2, K_g, λ_q, θ)
+    x_total_x(x, λ_u, K_u, λ_d, K_d, λ_g1, λ_g2, K_g, λ_q, K_q, θ)
 
 Total momentum density.
 """
 function xtotx(x::Float64, λ_u::Float64, K_u::Float64, λ_d::Float64, K_d::Float64,
-               λ_g1::Float64, λ_g2::Float64, K_g::Float64, λ_q::Float64, θ::Vector{Float64})
+    λ_g1::Float64, λ_g2::Float64, K_g::Float64, λ_q::Float64, K_q::Float64, θ::Vector{Float64})
 
     xuvx = x_uv_x(x, λ_u, K_u)
 
@@ -189,42 +191,42 @@ function xtotx(x::Float64, λ_u::Float64, K_u::Float64, λ_d::Float64, K_d::Floa
 
     # For ValencePDFParams
     if length(θ) == 7
-        
-        xgx = x_g_x(x, λ_g1, λ_g2, K_g, θ[1], θ[2])
 
-        xubarx = x_q_x(x, λ_q, θ[3])
-        
-        xdbarx = x_q_x(x, λ_q, θ[4])
-        
-        xsx = x_q_x(x, λ_q, θ[5])
-        
-        xcx = x_q_x(x, λ_q, θ[6])
-    
-        xbx = x_q_x(x, λ_q, θ[7])
+        xgx = x_g_x(x, λ_g1, λ_g2, K_g, K_q, θ[1], θ[2])
 
-    # For DirichletPDFParams
+        xubarx = x_q_x(x, λ_q, K_q, θ[3])
+
+        xdbarx = x_q_x(x, λ_q, K_q, θ[4])
+
+        xsx = x_q_x(x, λ_q, K_q, θ[5])
+
+        xcx = x_q_x(x, λ_q, K_q, θ[6])
+
+        xbx = x_q_x(x, λ_q, K_q, θ[7])
+
+        # For DirichletPDFParams
     elseif length(θ) == 9
 
-        xgx = x_g_x(x, λ_g1, λ_g2, K_g, θ[3], θ[4])
+        xgx = x_g_x(x, λ_g1, λ_g2, K_g, K_q, θ[3], θ[4])
 
-        xubarx = x_q_x(x, λ_q, θ[5])
-        
-        xdbarx = x_q_x(x, λ_q, θ[6])
-        
-        xsx = x_q_x(x, λ_q, θ[7])
-        
-        xcx = x_q_x(x, λ_q, θ[8])
-    
-        xbx = x_q_x(x, λ_q, θ[9])
+        xubarx = x_q_x(x, λ_q, K_q, θ[5])
+
+        xdbarx = x_q_x(x, λ_q, K_q, θ[6])
+
+        xsx = x_q_x(x, λ_q, K_q, θ[7])
+
+        xcx = x_q_x(x, λ_q, K_q, θ[8])
+
+        xbx = x_q_x(x, λ_q, K_q, θ[9])
 
     else
 
         @error "length(θ) must be 7 or 9 for a valid PDF parametrisation"
 
-    end 
-    
+    end
+
     return xuvx + xdvx + xgx + xubarx + xdbarx + xsx + xcx + xbx
-    
+
 end
 
 """
@@ -234,10 +236,10 @@ Total momentum density.
 """
 function xtotx(x::Float64, pdf_params::AbstractPDFParams)
 
-    pdf = pdf_params 
+    pdf = pdf_params
 
     return xtotx(x, pdf.λ_u, pdf.K_u, pdf.λ_d, pdf.K_d, pdf.λ_g1,
-                 pdf.λ_g2, pdf.K_g, pdf.λ_q, pdf.θ)
+        pdf.λ_g2, pdf.K_g, pdf.λ_q, pdf.K_q, pdf.θ)
 end
 
 """
@@ -246,51 +248,51 @@ end
 Total integrated momentum density. Should equal 1.
 """
 function int_xtotx(λ_u::Float64, K_u::Float64, λ_d::Float64, K_d::Float64,
-                   λ_g1::Float64, λ_g2::Float64, K_g::Float64, λ_q::Float64, θ::Array{Float64})
+    λ_g1::Float64, λ_g2::Float64, K_g::Float64, λ_q::Float64, K_q::Float64, θ::Array{Float64})
 
     A_u = 2 / sf.beta(λ_u, K_u + 1)
     A_d = 1 / sf.beta(λ_d, K_d + 1)
 
     # ValencePDFParams
     if length(θ) == 7
-        
+
         A_g1 = θ[1] / sf.beta(λ_g1 + 1, K_g + 1)
-        A_g2 = θ[2] / sf.beta(λ_g2 + 1, 5 + 1)
-    
-        A_ubar = (θ[3] / 2) / sf.beta(λ_q + 1, 5 + 1)
-        A_dbar = (θ[4] / 2) / sf.beta(λ_q + 1, 5 + 1)
+        A_g2 = θ[2] / sf.beta(λ_g2 + 1, K_q + 1)
 
-        A_s = (θ[5] / 2) / sf.beta(λ_q + 1, 5 + 1)
-        A_c = (θ[6] / 2) / sf.beta(λ_q + 1, 5 + 1)
-        A_b = (θ[7] / 2) / sf.beta(λ_q + 1, 5 + 1)
+        A_ubar = (θ[3] / 2) / sf.beta(λ_q + 1, K_q + 1)
+        A_dbar = (θ[4] / 2) / sf.beta(λ_q + 1, K_q + 1)
 
-    # DirichletPDFParams
+        A_s = (θ[5] / 2) / sf.beta(λ_q + 1, K_q + 1)
+        A_c = (θ[6] / 2) / sf.beta(λ_q + 1, K_q + 1)
+        A_b = (θ[7] / 2) / sf.beta(λ_q + 1, K_q + 1)
+
+        # DirichletPDFParams
     elseif length(θ) == 9
 
         A_g1 = θ[3] / sf.beta(λ_g1 + 1, K_g + 1)
-        A_g2 = θ[4] / sf.beta(λ_g2 + 1, 5 + 1)
-    
-    
-        A_ubar = (θ[5] / 2) / sf.beta(λ_q + 1, 5 + 1)
-        A_dbar = (θ[6] / 2) / sf.beta(λ_q + 1, 5 + 1)
+        A_g2 = θ[4] / sf.beta(λ_g2 + 1, K_q + 1)
 
-        A_s = (θ[7] / 2) / sf.beta(λ_q + 1, 5 + 1)
-        A_c = (θ[8] / 2) / sf.beta(λ_q + 1, 5 + 1)
-        A_b = (θ[9] / 2) / sf.beta(λ_q + 1, 5 + 1)
-    
+
+        A_ubar = (θ[5] / 2) / sf.beta(λ_q + 1, K_q + 1)
+        A_dbar = (θ[6] / 2) / sf.beta(λ_q + 1, K_q + 1)
+
+        A_s = (θ[7] / 2) / sf.beta(λ_q + 1, K_q + 1)
+        A_c = (θ[8] / 2) / sf.beta(λ_q + 1, K_q + 1)
+        A_b = (θ[9] / 2) / sf.beta(λ_q + 1, K_q + 1)
+
     else
-        
-         @error "length(θ) must be 7 or 9 for a valid PDF parametrisation"
 
-    end       
+        @error "length(θ) must be 7 or 9 for a valid PDF parametrisation"
+
+    end
 
     I_u = A_u * sf.beta(λ_u + 1, K_u + 1)
     I_d = A_d * sf.beta(λ_d + 1, K_d + 1)
-    I_g = A_g1 * sf.beta(λ_g1 + 1, K_g + 1) + A_g2 * sf.beta(λ_g2 + 1, 5 + 1)
-    I_q = 2 * (A_ubar + A_dbar + A_s + A_c + A_b) * sf.beta(λ_q + 1, 5 + 1)
-    
+    I_g = A_g1 * sf.beta(λ_g1 + 1, K_g + 1) + A_g2 * sf.beta(λ_g2 + 1, K_q + 1)
+    I_q = 2 * (A_ubar + A_dbar + A_s + A_c + A_b) * sf.beta(λ_q + 1, K_q + 1)
+
     I_tot = I_u + I_d + I_g + I_q
-    
+
     I_tot
 end
 
@@ -304,8 +306,8 @@ function int_xtotx(pdf_params::AbstractPDFParams)
     pdf = pdf_params
 
     result = int_xtotx(pdf.λ_u, pdf.K_u, pdf.λ_d, pdf.K_d,
-                       pdf.λ_g1, pdf.λ_g2, pdf.K_g, pdf.λ_q, pdf.θ)
-    
+        pdf.λ_g1, pdf.λ_g2, pdf.K_g, pdf.λ_q, pdf.K_q, pdf.θ)
+
     return result
 end
 
@@ -319,7 +321,7 @@ function plot_input_pdfs end
 
 
 function plot_input_pdfs(pdf_params::ValencePDFParams, xmin::Float64=1.0e-2,
-                         xmax::Float64=1.0, nx::Integer=1000)
+    xmax::Float64=1.0, nx::Integer=1000)
 
     x_grid = range(xmin, stop=xmax, length=nx)
     pdf = pdf_params
@@ -327,14 +329,14 @@ function plot_input_pdfs(pdf_params::ValencePDFParams, xmin::Float64=1.0e-2,
     p = plot(x_grid, [x_uv_x(x, pdf.λ_u, pdf.K_u) for x in x_grid], label="x uv(x)", lw=3)
     p = plot!(x_grid, [x_dv_x(x, pdf.λ_d, pdf.K_d) for x in x_grid], label="x dv(x)", lw=3)
 
-    p = plot!(x_grid, [x_g_x(x, pdf.λ_g1, pdf.λ_g2, pdf.K_g, pdf.θ[1], pdf.θ[2]) 
-                       for x in x_grid], label="x g(x)", lw=3)    
-    p = plot!(x_grid, [x_q_x(x, pdf.λ_q, pdf.θ[3]) for x in x_grid], label="x ubar(x)", lw=3)
-    p = plot!(x_grid, [x_q_x(x, pdf.λ_q, pdf.θ[4]) for x in x_grid], label="x dbar(x)", lw=3)
-    p = plot!(x_grid, [x_q_x(x, pdf.λ_q, pdf.θ[5]) for x in x_grid], label="x s(x)", lw=3)
-    p = plot!(x_grid, [x_q_x(x, pdf.λ_q, pdf.θ[6]) for x in x_grid], label="x c(x)", lw=3)
-    p = plot!(x_grid, [x_q_x(x, pdf.λ_q, pdf.θ[7]) for x in x_grid], label="x b(x)", lw=3)
-        
+    p = plot!(x_grid, [x_g_x(x, pdf.λ_g1, pdf.λ_g2, pdf.K_g, pdf.K_q, pdf.θ[1], pdf.θ[2])
+                       for x in x_grid], label="x g(x)", lw=3)
+    p = plot!(x_grid, [x_q_x(x, pdf.λ_q, pdf.K_q, pdf.θ[3]) for x in x_grid], label="x ubar(x)", lw=3)
+    p = plot!(x_grid, [x_q_x(x, pdf.λ_q, pdf.K_q, pdf.θ[4]) for x in x_grid], label="x dbar(x)", lw=3)
+    p = plot!(x_grid, [x_q_x(x, pdf.λ_q, pdf.K_q, pdf.θ[5]) for x in x_grid], label="x s(x)", lw=3)
+    p = plot!(x_grid, [x_q_x(x, pdf.λ_q, pdf.K_q, pdf.θ[6]) for x in x_grid], label="x c(x)", lw=3)
+    p = plot!(x_grid, [x_q_x(x, pdf.λ_q, pdf.K_q, pdf.θ[7]) for x in x_grid], label="x b(x)", lw=3)
+
     p = plot!(xlabel="x")
     p = plot!(xaxis=:log, yaxis=:log, legend=:outertopright)
     p = ylims!(1e-8, 30)
@@ -344,7 +346,7 @@ end
 
 
 function plot_input_pdfs(pdf_params::DirichletPDFParams, xmin::Float64=1.0e-2,
-                         xmax::Float64=1.0, nx::Integer=1000)
+    xmax::Float64=1.0, nx::Integer=1000)
 
     x_grid = range(xmin, stop=xmax, length=nx)
     pdf = pdf_params
@@ -352,13 +354,13 @@ function plot_input_pdfs(pdf_params::DirichletPDFParams, xmin::Float64=1.0e-2,
     p = plot(x_grid, [x_uv_x(x, pdf.λ_u, pdf.K_u) for x in x_grid], label="x uv(x)", lw=3)
     p = plot!(x_grid, [x_dv_x(x, pdf.λ_d, pdf.K_d) for x in x_grid], label="x dv(x)", lw=3)
 
-    p = plot!(x_grid, [x_g_x(x, pdf.λ_g1, pdf.λ_g2, pdf.K_g, pdf.θ[3], pdf.θ[4]) 
+    p = plot!(x_grid, [x_g_x(x, pdf.λ_g1, pdf.λ_g2, pdf.K_g, pdf.K_q, pdf.θ[3], pdf.θ[4])
                        for x in x_grid], label="x g(x)", lw=3)
-    p = plot!(x_grid, [x_q_x(x, pdf.λ_q, pdf.θ[5]) for x in x_grid], label="x ubar(x)", lw=3)
-    p = plot!(x_grid, [x_q_x(x, pdf.λ_q, pdf.θ[6]) for x in x_grid], label="x dbar(x)", lw=3)
-    p = plot!(x_grid, [x_q_x(x, pdf.λ_q, pdf.θ[7]) for x in x_grid], label="x s(x)", lw=3)
-    p = plot!(x_grid, [x_q_x(x, pdf.λ_q, pdf.θ[8]) for x in x_grid], label="x c(x)", lw=3)
-    p = plot!(x_grid, [x_q_x(x, pdf.λ_q, pdf.θ[9]) for x in x_grid], label="x b(x)", lw=3)
+    p = plot!(x_grid, [x_q_x(x, pdf.λ_q, pdf.K_q, pdf.θ[5]) for x in x_grid], label="x ubar(x)", lw=3)
+    p = plot!(x_grid, [x_q_x(x, pdf.λ_q, pdf.K_q, pdf.θ[6]) for x in x_grid], label="x dbar(x)", lw=3)
+    p = plot!(x_grid, [x_q_x(x, pdf.λ_q, pdf.K_q, pdf.θ[7]) for x in x_grid], label="x s(x)", lw=3)
+    p = plot!(x_grid, [x_q_x(x, pdf.λ_q, pdf.K_q, pdf.θ[8]) for x in x_grid], label="x c(x)", lw=3)
+    p = plot!(x_grid, [x_q_x(x, pdf.λ_q, pdf.K_q, pdf.θ[9]) for x in x_grid], label="x b(x)", lw=3)
 
     p = plot!(xlabel="x")
     p = plot!(xaxis=:log, yaxis=:log, legend=:outertopright)
@@ -379,7 +381,7 @@ function get_input_pdf_func end
 function get_input_pdf_func(pdf_params::ValencePDFParams)::Function
 
     pdf = pdf_params
-    
+
     func = function _input_pdfs(i, x)::Float64
         i = i[]
         x = x[]
@@ -388,7 +390,7 @@ function get_input_pdf_func(pdf_params::ValencePDFParams)::Function
 
         # gluon
         if (i == 0)
-            f = x_g_x(x, pdf.λ_g1, pdf.λ_g2, pdf.K_g, pdf.θ[1], pdf.θ[2]) 
+            f = x_g_x(x, pdf.λ_g1, pdf.λ_g2, pdf.K_g, pdf.K_q, pdf.θ[1], pdf.θ[2])
         end
 
         # u valence
@@ -403,27 +405,27 @@ function get_input_pdf_func(pdf_params::ValencePDFParams)::Function
 
         # ubar
         if (i == 3)
-            f = x_q_x(x, pdf.λ_q, pdf.θ[3])
+            f = x_q_x(x, pdf.λ_q, pdf.K_q, pdf.θ[3])
         end
 
         # dbar
         if (i == 4)
-            f = x_q_x(x, pdf.λ_q, pdf.θ[4])
+            f = x_q_x(x, pdf.λ_q, pdf.K_q, pdf.θ[4])
         end
 
         # s and sbar
         if (i == 5) || (i == 6)
-            f = x_q_x(x, pdf.λ_q, pdf.θ[5])
+            f = x_q_x(x, pdf.λ_q, pdf.K_q, pdf.θ[5])
         end
 
         # c and cbar
         if (i == 7) || (i == 8)
-            f = x_q_x(x, pdf.λ_q, pdf.θ[6])
+            f = x_q_x(x, pdf.λ_q, pdf.K_q, pdf.θ[6])
         end
 
         # d and dbar
         if (i == 9) || (i == 10)
-            f = x_q_x(x, pdf.λ_q, pdf.θ[7])
+            f = x_q_x(x, pdf.λ_q, pdf.K_q, pdf.θ[7])
         end
 
         return f
@@ -436,7 +438,7 @@ end
 function get_input_pdf_func(pdf_params::DirichletPDFParams)::Function
 
     pdf = pdf_params
-    
+
     func = function _input_pdfs(i, x)::Float64
         i = i[]
         x = x[]
@@ -445,7 +447,7 @@ function get_input_pdf_func(pdf_params::DirichletPDFParams)::Function
 
         # gluon
         if (i == 0)
-            f = x_g_x(x, pdf.λ_g1, pdf.λ_g2, pdf.K_g, pdf.θ[3], pdf.θ[4]) 
+            f = x_g_x(x, pdf.λ_g1, pdf.λ_g2, pdf.K_g, pdf.K_q, pdf.θ[3], pdf.θ[4])
         end
 
         # u valence
@@ -460,27 +462,27 @@ function get_input_pdf_func(pdf_params::DirichletPDFParams)::Function
 
         # ubar
         if (i == 3)
-            f = x_q_x(x, pdf.λ_q, pdf.θ[5])
+            f = x_q_x(x, pdf.λ_q, pdf.K_q, pdf.θ[5])
         end
 
         # dbar
         if (i == 4)
-            f = x_q_x(x, pdf.λ_q, pdf.θ[6])
+            f = x_q_x(x, pdf.λ_q, pdf.K_q, pdf.θ[6])
         end
 
         # s and sbar
         if (i == 5) || (i == 6)
-            f = x_q_x(x, pdf.λ_q, pdf.θ[7])
+            f = x_q_x(x, pdf.λ_q, pdf.K_q, pdf.θ[7])
         end
 
         # c and cbar
         if (i == 7) || (i == 8)
-            f = x_q_x(x, pdf.λ_q, pdf.θ[8])
+            f = x_q_x(x, pdf.λ_q, pdf.K_q, pdf.θ[8])
         end
 
         # d and dbar
         if (i == 9) || (i == 10)
-            f = x_q_x(x, pdf.λ_q, pdf.θ[9])
+            f = x_q_x(x, pdf.λ_q, pdf.K_q, pdf.θ[9])
         end
 
         return f
@@ -496,15 +498,15 @@ end
 The relevant mapping for use with QCDNUM and `get_input_pdf_func`.
 """
 #                         tb  bb  cb  sb  ub  db  g   d   u   s   c   b   t
-input_pdf_map = Float64.([0., 0., 0., 0.,-1., 0., 0., 0., 1., 0., 0., 0., 0., # 1 # U valence
-                          0., 0., 0., 0., 0.,-1., 0., 1., 0., 0., 0., 0., 0., # 2 # D valence
-                          0., 0., 0., 0., 1., 0., 0., 0., 0., 0., 0., 0., 0., # 3 # u sea
-                          0., 0., 0., 0., 0., 1., 0., 0., 0., 0., 0., 0., 0., # 4 # d sea
-                          0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0., 0., # 5 # s
-                          0., 0., 0., 1., 0., 0., 0., 0., 0., 0., 0., 0., 0., # 6 # sbar
-                          0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0., # 7 # c
-                          0., 0., 1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., # 8 # cbar
-                          0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., # 9 # b
-                          0., 1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., # 10 # bbar
-                          0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., # 11
-                          0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.]); # 12
+input_pdf_map = Float64.([0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, # 1 # U valence
+    0.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, # 2 # D valence
+    0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, # 3 # u sea
+    0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, # 4 # d sea
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, # 5 # s
+    0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, # 6 # sbar
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, # 7 # c
+    0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, # 8 # cbar
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, # 9 # b
+    0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, # 10 # bbar
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, # 11
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]); # 12
