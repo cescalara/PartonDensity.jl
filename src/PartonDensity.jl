043228355fc4f036e1044955af12064433a368af
Francesca Capel <capel.francesca@gmail.com>
2022-09-01 17:23:22 +0200

    Add back requires, fix docs later

--- src/PartonDensity.jl
+++ src/PartonDensity.jl
@@ -6,10 +6,14 @@ include("cross_section.jl")
 include("forward_model.jl")
 include("forward_model_sysErr.jl")
 include("zeus.jl")
-include("fit.jl")
 include("Bernstein_parametrisation.jl")
 include("BernDir_parametrisation.jl")
 include("Bernstein_forward_model.jl")
 include("Bernstein_fit.jl")
 
+end
+
+using Requires
+function __init__()
+    @require BAT = "c0cd4b16-88b7-57fa-983b-ab80aecada7e" include("fit.jl")
 end
