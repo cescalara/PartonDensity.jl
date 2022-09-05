33e1e855b0310feef0ef7903d9f66d40977dcca7
Francesca Capel <capel.francesca@gmail.com>
2022-09-05 15:31:32 +0200

    Fix tests and @requires loading for Bernstein_fit too

--- src/PartonDensity.jl
+++ src/PartonDensity.jl
@@ -9,11 +9,11 @@ include("zeus.jl")
 include("Bernstein_parametrisation.jl")
 include("BernDir_parametrisation.jl")
 include("Bernstein_forward_model.jl")
-include("Bernstein_fit.jl")
-
-end
 
 using Requires
 function __init__()
     @require BAT = "c0cd4b16-88b7-57fa-983b-ab80aecada7e" include("fit.jl")
+    @require BAT = "c0cd4b16-88b7-57fa-983b-ab80aecada7e" include("Bernstein_fit.jl")
+end
+
 end
