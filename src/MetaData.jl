961e51bbcb903505d14359acd07c79cd27de5ecb
Andrii Verbytskyi <andriish@pcatlas18.mpp.mpg.de>
2023-09-04 16:17:48 +0200

    Separate Metadata

--- src/MetaData.jl
+++ src/MetaData.jl
@@ -1,4 +1,4 @@
-export MataData
+export MetaData
 
 struct MetaData
  name::String
--- src/PartonDensity.jl
+++ src/PartonDensity.jl
@@ -10,7 +10,7 @@ using Plots
 include("parton.jl")
 include("parametrisations/parametrisations.jl")
 include("cross_section.jl")
-include("MataData.jl")
+include("MetaData.jl")
 include("zeus.jl")
 include("../examples/data/zeus_high_x.jl")
 include("forward_model.jl")
