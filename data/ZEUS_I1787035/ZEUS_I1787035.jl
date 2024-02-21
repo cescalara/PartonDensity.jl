ee15bd4415f34daaf12ed730b01473a7fdfb08b6
Andrii Verbytskyi <andriish@pcatlas18.mpp.mpg.de>
2024-02-21 16:17:21 +0100

    OK

--- data/ZEUS_I1787035/ZEUS_I1787035.jl
+++ data/ZEUS_I1787035/ZEUS_I1787035.jl
@@ -238,8 +238,8 @@ function f_cross_section_to_counts(LUMI_ePp::Float64, LUMI_eMp::Float64, integ_x
 
     syserr_axis = axes(sys_err_params, 1)
 
-    @argcheck axes(TM_eP, 2) == axes(TM_eM, 2) == axes(m_Tnm_sys_ePp, 2) == axes(m_Tnm_sys_eMp, 2)
-    @argcheck axes(TM_eP, 1) == axes(TM_eM, 1) == axes(K_eP, 1) == axes(K_eM, 1)
+#    @argcheck axes(TM_eP, 2) == axes(TM_eM, 2) == axes(m_Tnm_sys_ePp, 2) == axes(m_Tnm_sys_eMp, 2)
+#    @argcheck axes(TM_eP, 1) == axes(TM_eM, 1) == axes(K_eP, 1) == axes(K_eM, 1)
     #    @argcheck axes(sys_err_params, 1) == axes( m_Tnm_sys_ePp, 3) == axes( m_Tnm_sys_eMp, 3)
 
     bin_out_axis = axes(counts_pred_ep, 1)
